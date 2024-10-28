97120d00c54e9c30bd610858166642daa9395b76
Marshal Wu <marshal.wu@gmail.com>
2024-10-28 11:41:18 +0000

    update

--- build_rstudio.sh
+++ build_rstudio.sh
@@ -3,6 +3,7 @@
 git clone https://github.com/rocker-org/rocker-versioned2.git
 
 cp patches/dockerfiles/rstudio.Dockerfile rocker-versioned2/dockerfiles/my-rstudio.Dockerfile
+cp patches/install_R_source.sh rocker-versioned2/scripts
 
 cd rocker-versioned2
 
